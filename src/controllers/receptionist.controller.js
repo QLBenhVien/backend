76c2d6340c767dead40437b9d985cb15adaf4850
ChiNg22nd04 <dedonai99@gmail.com>
2024-10-16 23:45:30 +0700

    eanw

--- src/controllers/receptionist.controller.js
+++ src/controllers/receptionist.controller.js
@@ -1,8 +1,4 @@
-const {
-  successResponse,
-  errorResponse,
-  validateObjectId,
-} = require("../helpers/index");
+const { successResponse, errorResponse, validateObjectId } = require("../helpers/index");
 const Khoa = require("../models/Khoa");
 const LichDatKham = require("../models/LichDatKham");
 const NhanVien = require("../models/NhanVien");
@@ -11,290 +7,289 @@ const ChucVu = require("../models/ChucVu");
 const PhieuKham = require("../models/PhieuKham");
 
 function ReceptionistController() {
-  // đặt lịch
-  this.scheduleappointment = async (req, res, next) => {
-    try {
-      const { MaBS, MaBN, MaKhoa, TrieuChung, NgayDatKham } = req.body;
-      const receptionisterId = req.authenticatedUser.userId;
+	// đặt lịch
+	this.scheduleappointment = async (req, res, next) => {
+		try {
+			const { MaBS, MaBN, MaKhoa, TrieuChung, NgayDatKham } = req.body;
+			const receptionisterId = req.authenticatedUser.userId;
 
-      // Kiểm tra tính hợp lệ của các ID
-      if (!validateObjectId(MaKhoa, "Mã khoa", res)) return;
-      if (!validateObjectId(MaBN, "Mã bệnh nhân", res)) return;
-      if (!validateObjectId(MaBS, "Mã bác sĩ", res)) return;
+			// Kiểm tra tính hợp lệ của các ID
+			if (!validateObjectId(MaKhoa, "Mã khoa", res)) return;
+			if (!validateObjectId(MaBN, "Mã bệnh nhân", res)) return;
+			if (!validateObjectId(MaBS, "Mã bác sĩ", res)) return;
 
-      // Kiểm tra NgayDatKham có hợp lệ và là ngày trong tương lai không
-      const parsedDate = Date.parse(NgayDatKham);
-      if (isNaN(parsedDate)) {
-        return res.status(400).json({ message: "Ngày đặt khám không hợp lệ" });
-      }
+			// Kiểm tra NgayDatKham có hợp lệ và là ngày trong tương lai không
+			const parsedDate = Date.parse(NgayDatKham);
+			if (isNaN(parsedDate)) {
+				return res.status(400).json({ message: "Ngày đặt khám không hợp lệ" });
+			}
 
-      const date = new Date(parsedDate);
-      if (date <= new Date()) {
-        return res
-          .status(400)
-          .json({ message: "Ngày đặt khám phải là ngày trong tương lai" });
-      }
+			const date = new Date(parsedDate);
+			if (date <= new Date()) {
+				return res
+					.status(400)
+					.json({ message: "Ngày đặt khám phải là ngày trong tương lai" });
+			}
 
-      // Kiểm tra sự tồn tại của MaKhoa, MaBN, và MaBS
-      const khoa = await Khoa.findById(MaKhoa);
-      if (!khoa) {
-        return errorResponse(req, res, "Khoa không tồn tại", 404);
-      }
+			// Kiểm tra sự tồn tại của MaKhoa, MaBN, và MaBS
+			const khoa = await Khoa.findById(MaKhoa);
+			if (!khoa) {
+				return errorResponse(req, res, "Khoa không tồn tại", 404);
+			}
 
-      const benhNhan = await BenhNhan.findById(MaBN);
-      if (!benhNhan) {
-        return errorResponse(req, res, "Bệnh nhân không tồn tại", 404);
-      }
+			const benhNhan = await BenhNhan.findById(MaBN);
+			if (!benhNhan) {
+				return errorResponse(req, res, "Bệnh nhân không tồn tại", 404);
+			}
 
-      let hasBS = false;
-      if (MaBS && MaBS.trim() !== "") {
-        const bacSi = await NhanVien.findById(MaBS).populate("MaCV");
-        if (!bacSi) {
-          return errorResponse(req, res, "Bác sĩ này không tồn tại", 404);
-        }
-        if (bacSi.MaCV && bacSi.MaCV.TenCV === "Bác Sĩ") {
-          hasBS = true;
-        } else {
-          return errorResponse(req, res, "Nhân viên không phải là bác sĩ", 400);
-        }
-      }
+			let hasBS = false;
+			if (MaBS && MaBS.trim() !== "") {
+				const bacSi = await NhanVien.findById(MaBS).populate("MaCV");
+				if (!bacSi) {
+					return errorResponse(req, res, "Bác sĩ này không tồn tại", 404);
+				}
+				if (bacSi.MaCV && bacSi.MaCV.TenCV === "Bác Sĩ") {
+					hasBS = true;
+				} else {
+					return errorResponse(req, res, "Nhân viên không phải là bác sĩ", 400);
+				}
+			}
 
-      // Lấy ngày và tính SoThuTuKham
-      const dateStart = new Date(date);
-      dateStart.setHours(0, 0, 0, 0);
-      const dateEnd = new Date(date);
-      dateEnd.setHours(23, 59, 59, 999);
+			// Lấy ngày và tính SoThuTuKham
+			const dateStart = new Date(date);
+			dateStart.setHours(0, 0, 0, 0);
+			const dateEnd = new Date(date);
+			dateEnd.setHours(23, 59, 59, 999);
 
-      const lastLichKham = await LichDatKham.findOne({
-        NgayDatKham: { $gte: dateStart, $lte: dateEnd },
-      }).sort({ SoThuTuKham: -1 });
+			const lastLichKham = await LichDatKham.findOne({
+				NgayDatKham: { $gte: dateStart, $lte: dateEnd },
+			}).sort({ SoThuTuKham: -1 });
 
-      let sttKham = lastLichKham ? lastLichKham.SoThuTuKham + 1 : 1;
+			let sttKham = lastLichKham ? lastLichKham.SoThuTuKham + 1 : 1;
 
-      // Tạo lịch đặt khám mới
-      const lichDatKhamMoi = new LichDatKham({
-        BacSiID: hasBS ? MaBS : null,
-        KhoaID: MaKhoa,
-        BenhNhanID: MaBN,
-        NhanVienTaoLich: receptionisterId,
-        TrieuChung: TrieuChung,
-        NgayDatKham: date,
-        SoThuTuKham: sttKham,
-      });
+			// Tạo lịch đặt khám mới
+			const lichDatKhamMoi = new LichDatKham({
+				BacSiID: hasBS ? MaBS : null,
+				KhoaID: MaKhoa,
+				BenhNhanID: MaBN,
+				NhanVienTaoLich: receptionisterId,
+				TrieuChung: TrieuChung,
+				NgayDatKham: date,
+				SoThuTuKham: sttKham,
+			});
 
-      await lichDatKhamMoi.save();
+			await lichDatKhamMoi.save();
 
-      if (lichDatKhamMoi.TrangThai) {
-        const phieuKhamMoi = new PhieuKham({
-          MaPhieu: `PK${Date.now()}`,
-          MaDanhSach: lichDatKhamMoi._id, // Tham chiếu đến lịch đặt khám vừa tạo
-          MaBenhNhan: MaBN,
-          MaNhanVien: receptionisterId, // Nhân viên tạo phiếu khám
-          TenPhieu: "Phiếu khám bệnh", // Tên phiếu
-          SoThuTuKham: sttKham,
-          NgayKham: date, // Cung cấp giá trị cho NgayKham, sử dụng giá trị date từ trước
-        });
+			if (lichDatKhamMoi.TrangThai) {
+				const phieuKhamMoi = new PhieuKham({
+					MaPhieu: `PK${Date.now()}`,
+					MaDanhSach: lichDatKhamMoi._id, // Tham chiếu đến lịch đặt khám vừa tạo
+					MaBenhNhan: MaBN,
+					MaNhanVien: receptionisterId, // Nhân viên tạo phiếu khám
+					TenPhieu: "Phiếu khám bệnh", // Tên phiếu
+					SoThuTuKham: sttKham,
+					NgayKham: date, // Cung cấp giá trị cho NgayKham, sử dụng giá trị date từ trước
+				});
 
-        await phieuKhamMoi.save();
-      }
+				await phieuKhamMoi.save();
+			}
 
-      return successResponse(
-        req,
-        res,
-        {
-          message: "Lịch khám đã được tạo thành công",
-          lichDatKham: lichDatKhamMoi,
-          phieuKham: lichDatKhamMoi.TrangThai ? phieuKhamMoi : null, // Chỉ trả về phieuKham nếu trạng thái là true
-        },
-        201
-      );
-    } catch (error) {
-      console.error("Error when scheduling appointment: ", error);
-      return errorResponse(req, res, "Lỗi server khi tạo lịch khám.");
-    }
-  };
+			return successResponse(
+				req,
+				res,
+				{
+					message: "Lịch khám đã được tạo thành công",
+					lichDatKham: lichDatKhamMoi,
+					phieuKham: lichDatKhamMoi.TrangThai ? phieuKhamMoi : null, // Chỉ trả về phieuKham nếu trạng thái là true
+				},
+				201
+			);
+		} catch (error) {
+			console.error("Error when scheduling appointment: ", error);
+			return errorResponse(req, res, "Lỗi server khi tạo lịch khám.");
+		}
+	};
 
-  this.approveappointment = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const receptionisterId = req.authenticatedUser.userId;
+	this.approveappointment = async (req, res) => {
+		try {
+			const { id } = req.params;
+			const receptionisterId = req.authenticatedUser.userId;
 
-      // Kiểm tra tính hợp lệ của các ID
-      if (!validateObjectId(appointmentID, "Mã cuộc hẹn", res)) return;
-      // Kiểm tra xem MaBN có tồn tại trong bảng NhanVien hay không
-      const appointment = await LichDatKham.findById(appointmentID);
-      if (!appointment) {
-        return errorResponse(req, res, "Lịch khám này không tồn tại", 404);
-      }
+			// Kiểm tra tính hợp lệ của các ID
+			if (!validateObjectId(id, "Mã cuộc hẹn", res)) return;
+			// Kiểm tra xem MaBN có tồn tại trong bảng NhanVien hay không
+			const appointment = await LichDatKham.findOne(id);
+			if (!appointment) {
+				return errorResponse(req, res, "Lịch khám này không tồn tại", 404);
+			}
 
-      // Cập nhật trạng thái của lịch khám
-      appointment.TrangThai = true;
-      await appointment.save();
+			// Cập nhật trạng thái của lịch khám
+			appointment.TrangThai = true;
+			await appointment.save();
 
-      // Tạo phiếu khám nếu trạng thái là true
-      const phieuKhamMoi = new PhieuKham({
-        MaPhieu: `PK${Date.now()}`,
-        MaDanhSach: appointment._id,
-        MaBenhNhan: appointment.BenhNhanID,
-        MaNhanVien: receptionisterId,
-        TenPhieu: "Phiếu khám bệnh",
-        SoThuTuKham: appointment.SoThuTuKham,
-        NgayKham: appointment.NgayDatKham,
-      });
+			// Tạo phiếu khám nếu trạng thái là true
+			const phieuKhamMoi = new PhieuKham({
+				MaPhieu: `PK${Date.now()}`,
+				MaDanhSach: appointment._id,
+				MaBenhNhan: appointment.BenhNhanID,
+				MaNhanVien: receptionisterId,
+				TenPhieu: "Phiếu khám bệnh",
+				SoThuTuKham: appointment.SoThuTuKham,
+				NgayKham: appointment.NgayDatKham,
+			});
 
-      await phieuKhamMoi.save();
+			await phieuKhamMoi.save();
 
-      return successResponse(
-        req,
-        res,
-        {
-          message:
-            "Lịch khám đã được xác nhận thành công và phiếu khám đã được tạo.",
-          lichDatKham: appointment,
-          phieuKham: phieuKhamMoi, // Trả về phiếu khám đã tạo
-        },
-        200
-      );
-    } catch (error) {
-      return errorResponse(req, res, "Lỗi server khi xác nhận lịch khám.");
-    }
-  };
+			return successResponse(
+				req,
+				res,
+				{
+					message: "Lịch khám đã được xác nhận thành công và phiếu khám đã được tạo.",
+					lichDatKham: appointment,
+					phieuKham: phieuKhamMoi, // Trả về phiếu khám đã tạo
+				},
+				200
+			);
+		} catch (error) {
+			return errorResponse(req, res, "Lỗi server khi xác nhận lịch khám.");
+		}
+	};
 
-  // huy lich kham
-  this.cancelappointment = async (req, res) => {
-    try {
-      const { id } = req.body;
-      console.log(id);
-      // Kiểm tra tính hợp lệ của các ID
-      if (!validateObjectId(id, "Mã cuộc hẹn", res)) return;
-      // Kiểm tra xem MaBN có tồn tại trong bảng NhanVien hay không
-      const appointment = await LichDatKham.findById({ id });
-      if (!appointment) {
-        return errorResponse(req, res, "Lịch khám này không tồn tại", 404);
-      }
+	// huy lich kham
+	this.cancelappointment = async (req, res) => {
+		try {
+			const { id } = req.body;
+			console.log(id);
+			// Kiểm tra tính hợp lệ của các ID
+			if (!validateObjectId(id, "Mã cuộc hẹn", res)) return;
+			// Kiểm tra xem MaBN có tồn tại trong bảng NhanVien hay không
+			const appointment = await LichDatKham.findById({ id });
+			if (!appointment) {
+				return errorResponse(req, res, "Lịch khám này không tồn tại", 404);
+			}
 
-      appointment.TrangThai = "canceled";
-      await appointment.save();
-      return successResponse(
-        req,
-        res,
-        {
-          message: "Lịch khám đã được hủy thành công",
-        },
-        200
-      );
-    } catch (error) {
-      return errorResponse(req, res, "Lỗi server khi xác nhận lịch khám.", 500);
-    }
-  };
+			appointment.TrangThai = "canceled";
+			await appointment.save();
+			return successResponse(
+				req,
+				res,
+				{
+					message: "Lịch khám đã được hủy thành công",
+				},
+				200
+			);
+		} catch (error) {
+			return errorResponse(req, res, "Lỗi server khi xác nhận lịch khám.", 500);
+		}
+	};
 
-  // cap nhap lich kham
-  this.updateAppointment = async (req, res) => {
-    try {
-      const { appointmentID } = req.params;
-      const receptionisterId = req.authenticatedUser.userId;
-      // Kiểm tra tính hợp lệ của các ID
-      if (!validateObjectId(appointmentID, "Mã cuộc hẹn", res)) return;
+	// cap nhap lich kham
+	this.updateAppointment = async (req, res) => {
+		try {
+			const { appointmentID } = req.params;
+			const receptionisterId = req.authenticatedUser.userId;
+			// Kiểm tra tính hợp lệ của các ID
+			if (!validateObjectId(appointmentID, "Mã cuộc hẹn", res)) return;
 
-      const { action } = req.body; // Lấy hành động từ body (action: "approve" hoặc "cancel")
-      if (!["approve", "cancel"].includes(action)) {
-        return errorResponse(req, res, "Hành động không hợp lệ", 400);
-      }
+			const { action } = req.body; // Lấy hành động từ body (action: "approve" hoặc "cancel")
+			if (!["approve", "cancel"].includes(action)) {
+				return errorResponse(req, res, "Hành động không hợp lệ", 400);
+			}
 
-      // Kiểm tra xem MaBN có tồn tại trong bảng LichDatKham hay không
-      const appointment = await LichDatKham.findById(appointmentID);
-      if (!appointment) {
-        return errorResponse(req, res, "Lịch khám này không tồn tại", 404);
-      }
-      let returnMsg = "";
-      // Xử lý dựa trên hành động
-      if (action === "approve") {
-        // Xử lý duyệt lịch hẹn
-        appointment.TrangThai = true;
-        returnMsg = "xác nhận";
-      } else if (action === "cancel") {
-        // Xử lý hủy lịch hẹn
-        appointment.TrangThai = false;
-        returnMsg = "hủy";
-      }
-      await appointment.save();
-      return successResponse(
-        req,
-        res,
-        {
-          message: `Lịch khám đã được ${returnMsg} thành công`,
-          lichDatKham: appointment,
-        },
-        200
-      );
-    } catch (error) {
-      return errorResponse(req, res, "Lỗi server khi xác nhận lịch khám.");
-    }
-  };
+			// Kiểm tra xem MaBN có tồn tại trong bảng LichDatKham hay không
+			const appointment = await LichDatKham.findById(appointmentID);
+			if (!appointment) {
+				return errorResponse(req, res, "Lịch khám này không tồn tại", 404);
+			}
+			let returnMsg = "";
+			// Xử lý dựa trên hành động
+			if (action === "approve") {
+				// Xử lý duyệt lịch hẹn
+				appointment.TrangThai = true;
+				returnMsg = "xác nhận";
+			} else if (action === "cancel") {
+				// Xử lý hủy lịch hẹn
+				appointment.TrangThai = false;
+				returnMsg = "hủy";
+			}
+			await appointment.save();
+			return successResponse(
+				req,
+				res,
+				{
+					message: `Lịch khám đã được ${returnMsg} thành công`,
+					lichDatKham: appointment,
+				},
+				200
+			);
+		} catch (error) {
+			return errorResponse(req, res, "Lỗi server khi xác nhận lịch khám.");
+		}
+	};
 
-  // list all lich kham
-  this.listLichdat = async (req, res) => {
-    try {
-      const dataDatkham = await LichDatKham.find({ TrangThai: false });
+	// list all lich kham
+	this.listLichdat = async (req, res) => {
+		try {
+			const dataDatkham = await LichDatKham.find({ TrangThai: false });
 
-      const datakham = [];
+			const datakham = [];
 
-      for (const dat of dataDatkham) {
-        const benhNhan = await BenhNhan.findOne({ _id: dat.BenhNhanID });
-        datakham.push({
-          id: dat._id,
-          idBn: dat.BenhNhanID,
-          TenBN: benhNhan ? benhNhan.Ten : "Không tìm thấy bệnh nhân", // Lấy tên bệnh nhân nếu tìm thấy
-          NgayDat: dat.NgayDatKham,
-          TrangThai: dat.TrangThai,
-        });
-      }
+			for (const dat of dataDatkham) {
+				const benhNhan = await BenhNhan.findOne({ _id: dat.BenhNhanID });
+				datakham.push({
+					id: dat._id,
+					idBn: dat.BenhNhanID,
+					TenBN: benhNhan ? benhNhan.Ten : "Không tìm thấy bệnh nhân", // Lấy tên bệnh nhân nếu tìm thấy
+					NgayDat: dat.NgayDatKham,
+					TrangThai: dat.TrangThai,
+				});
+			}
 
-      return successResponse(req, res, { Datkham: datakham }, 200);
-    } catch (error) {
-      console.error(error); // Log lỗi để kiểm tra
-      return errorResponse(req, res, "Lỗi server", 500);
-    }
-  };
+			return successResponse(req, res, { Datkham: datakham }, 200);
+		} catch (error) {
+			console.error(error); // Log lỗi để kiểm tra
+			return errorResponse(req, res, "Lỗi server", 500);
+		}
+	};
 
-  this.detailAppointment = async (req, res) => {
-    const { id } = req.params;
-    try {
-      console.log(id);
-      const appointment = await LichDatKham.findOne({ _id: id });
+	this.detailAppointment = async (req, res) => {
+		const { id } = req.params;
+		try {
+			console.log(id);
+			const appointment = await LichDatKham.findOne({ _id: id });
 
-      const BacSi = await NhanVien.findOne({ _id: appointment.BacSiID });
-      const BenhNhannew = await BenhNhan.findOne({
-        _id: appointment.BenhNhanID,
-      });
-      // const Khoanew = await Khoa.findOne({
-      //   _id: appointment.KhoaID,
-      // });
+			const BacSi = await NhanVien.findOne({ _id: appointment.BacSiID });
+			const BenhNhannew = await BenhNhan.findOne({
+				_id: appointment.BenhNhanID,
+			});
+			// const Khoanew = await Khoa.findOne({
+			//   _id: appointment.KhoaID,
+			// });
 
-      return successResponse(
-        req,
-        res,
-        {
-          detailDatkham: {
-            TenBN: BenhNhannew.Ten,
-            GioiTinh: BenhNhannew.GioiTinh,
-            MaBN: BenhNhannew._id,
-            TenBS: BacSi.HoTen,
-            NgayHen: appointment.NgayDatKham,
-            Ca: appointment.CaKham,
-            DiaChi: BenhNhannew.DiaChi,
-            TrieuChung: appointment.TrieuChung,
-            SDT: BenhNhannew.SDT,
-          },
-        },
-        200
-      );
-    } catch (error) {
-      console.error(error); // Log lỗi để kiểm tra
-      return errorResponse(req, res, "Lỗi server", 500);
-    }
-  };
-  return this;
+			return successResponse(
+				req,
+				res,
+				{
+					detailDatkham: {
+						TenBN: BenhNhannew.Ten,
+						GioiTinh: BenhNhannew.GioiTinh,
+						MaBN: BenhNhannew._id,
+						TenBS: BacSi.HoTen,
+						NgayHen: appointment.NgayDatKham,
+						Ca: appointment.CaKham,
+						DiaChi: BenhNhannew.DiaChi,
+						TrieuChung: appointment.TrieuChung,
+						SDT: BenhNhannew.SDT,
+					},
+				},
+				200
+			);
+		} catch (error) {
+			console.error(error); // Log lỗi để kiểm tra
+			return errorResponse(req, res, "Lỗi server", 500);
+		}
+	};
+	return this;
 }
 
 module.exports = ReceptionistController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const userRoute = require("./user.routes");
 const authRoute = require("./auth.routes");
 const adminRoute = require("./admin.routes");
-const doctorRouter = require("./doctor.routes");
+// const doctorRouter = require("./doctor.routes");
 const router = express.Router();
 const receptionistRoute = require("./receptionist.routes");
 const itSupportRoute = require("./itSupport.routes");
@@ -11,7 +11,7 @@ const itSupportRoute = require("./itSupport.routes");
 router.use("/", authRoute);
 router.use("/user", userRoute);
 router.use("/admin", adminRoute);
-router.use("/doctor", doctorRouter);
+// router.use("/doctor", doctorRouter);
 router.use("/itSupport", itSupportRoute);
 router.use("/receptionist", receptionistRoute);
 

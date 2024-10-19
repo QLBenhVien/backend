4cee881abf7a891a6e1fe08bfec6cd988d72ffba
ChiNg22nd04 <dedonai99@gmail.com>
2024-10-19 17:02:04 +0700

    update model account, itSupportController

--- src/models/ChucVu.js
+++ src/models/ChucVu.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const chucvuSchema = new mongoose.Schema({
-  TenCV: { type: String, required: true }, // Tên chức vụ, ví dụ "Bác sĩ"
-  Luong: { type: Number, required: true }, // Lương có kiểu là Number
+	TenCV: { type: String, required: true }, // Tên chức vụ, ví dụ "Bác sĩ"
+	Luong: { type: Number, required: true }, // Lương có kiểu là Number
 });
 
 const ChucVu = mongoose.model("ChucVu", chucvuSchema);
